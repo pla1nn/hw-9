aa69c2044ffdca030febbffb47f51662421364a9
pla1n <danilkuisis@gmail.com>
2024-08-04 15:30:49 +0300

    asD

--- src/js/task4.js
+++ src/js/task4.js
@@ -6,10 +6,18 @@ const list = document.querySelector('.bookmark__list');
 
 function createBookmark() {
     list.innerHTML = '';
-    bookmarks.forEach((bookmark) => {
+    bookmarks.forEach((bookmark, index) => {
         list.innerHTML += `<li class="bookmark__item">
-        <a href="${bookmark}" target="_blank" class="bookmark__url">${bookmark}</a><button class="delete__button">видалити</button>
+            <a href="${bookmark}" target="_blank" class="bookmark__url">${bookmark}</a>
+            <button data-index="${index}" class="delete__button">видалити</button>
         </li>`;
+    });
+
+    document.querySelectorAll('.delete__button').forEach(button => {
+        button.addEventListener('click', function() {
+            const index = this.getAttribute('data-index');
+            deleteBookmark(index);
+        });
     });
 }
 
@@ -25,49 +33,10 @@ function addBoomkark() {
     }
 }
 
+function deleteBookmark(i) {
+    bookmarks.splice(i, 1);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    createBookmark();
+}
+
 createBookmark();
-
-// const deleteButton = document.querySelector('.delete__button');
-
-// deleteButton.addEventListener('click', deleteBookmark);
-
-
-
-
-
-function getRandomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-
-    return `rgb(${r}, ${g}, ${b})`;
-}
-
-const links = document.querySelectorAll('.bookmark__url');
-
-links.forEach(link => {
-    link.style.backgroundColor = getRandomLightColor();
-});
-
-
-// ВСЕ ЩО ВНИЗУ, Я НЕ САМ ПИСАВ, ТО ШАБЛОН ПРОСТО ДЛЯ КРАСИ ЩОБ НЕ ВИЛIЗАЛИ ТЕМНI КОЛЬОРИ
-
-
-function getLuminance(r, g, b) {
-    const a = [r, g, b].map(function (v) {
-        v /= 255;
-        return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
-    });
-    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
-}
-
-function getRandomLightColor() {
-    let r, g, b, luminance;
-    do {
-        r = Math.floor(Math.random() * 256);
-        g = Math.floor(Math.random() * 256);
-        b = Math.floor(Math.random() * 256);
-        luminance = getLuminance(r, g, b);
-    } while (luminance < 0.5);
-    return `rgb(${r}, ${g}, ${b})`;
-}
